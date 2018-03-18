1590a301c19d3fd03de8ca417bd13ccde879a7d0
Juri Solovjov <jsolovjo@redhat.com>
2018-03-19 00:03:30 +0100

    added new classes

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -1,5 +1,6 @@
 package com.myapp;
 
+
 /**
  * Hello world!
  *
@@ -9,5 +10,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Bicycle first_bicycle = new Bicycle(3,21,45);
+        first_bicycle.setGear(24);
+        System.out.println("Speed is " + first_bicycle.getSpeed() + " and the gear is " + first_bicycle.getGear());
+
+        MountainBike second_bicycle = new MountainBike(3,5,21,45);
+        System.out.println("Second bicycle speed is " + second_bicycle.getSpeed() + " and seat height is " + second_bicycle.getSeatHeight());
+
     }
+
 }
+
